a7eaeeb8b7143dfda42d150718583d9f249486fe
Yang Ding <dyt811@gmail.com>
2019-09-11 17:33:05 -0400

    Add content from Tutorial III

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from configfile import ConfigClass
 
 app = Flask(__name__)
+
+# Load configuration from the object class.
+app.config.from_object(ConfigClass)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,35 @@
 from app import app
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 # Main root rout.
 @app.route("/")
 
 # index page rout.
 @app.route("/index")
-
-# The main function that was executed.
 def index():
-    return "Hello, World!"
+    # The main function that was executed.
+    user = {"username": "Data Structure"}
+    posts = [
+        {"author": {"username": "John"}, "body": "Nice!"},
+        {"author": {"username": "Susan"}, "body": "Averages!"},
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # Instantiate form data object.
+    form = LoginForm()
+
+    if form.validate_on_submit():
+
+        # Flash warning required.
+        flash(
+            f"Login requested for user {form.username.data}, rememebr_me={form.remember_me.data}"
+        )
+
+        # Redirect to a page should the form pass validation.
+        return redirect(url_for("index"))
+
+    return render_template("login.html", title="Sign In", form=form)
