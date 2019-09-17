213c7c1f27ba6fbd2bea6f9564a0d7ec357c815a
Yang Ding <dyt811@gmail.com>
2019-09-17 17:35:31 -0400

    Midway through Tutorial Part VI

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 
 @login.user_loader
@@ -27,14 +28,64 @@ class User(UserMixin, db.Model):
 
     # Hash generation.
     def set_password(self, password):
+        """
+        Function to set the password for the user database class
+        :param password:
+        :return:
+        """
         self.password_hash = generate_password_hash(password)
 
     # hash checking.
     def check_password(self, password):
+        """
+        Function to check the password for the user database class
+        :param password:
+        :return:
+        """
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self):
+        """
+        Return the avatar up to a particular size.
+        :param size:
+        :return:
+        """
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        link = f"https://www.gravatar.com/avatar/{digest}?d=identicon&s=128"
+        return link
+
+
+class Entry(db.Model):
+    """
+    This is used to model the data entry from the user regarding the scans.
+    """
+
+    id = db.Column(db.Integer, primary_key=True)
+    MRN = db.Column(db.Integer)
+    CNBPID = db.Column(db.String(10))
+    birth_weight = db.Column(db.String(140))
+    birth_date = db.Column(db.Date)
+    birth_time = db.Column(db.Time)
+    mri_date = db.Column(db.Date)
+    mri_reason = db.Column(db.String)
+
+    # Many more fields to add here.
+    mri_dx = db.Column(db.String)
+
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("user.id")
+    )  # used to associate who entered this entry.
+
+    def __repr__(self):
+        return "<Entry {}>".format(self.body)
 
 
 class Post(db.Model):
+    """
+    This is the post class, used to model the data brought on
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,23 @@
 from app import app, db
-from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm, RegistrationForm
-from flask_login import current_user, login_user, logout_user, login_required
+from flask import (
+    render_template,  # flask function to render a HTML template with elements replaced.
+    flash,  # show a message overlay.
+    redirect,  # redirect to another page.
+    url_for,  # used to interpret endpoints.
+    request,
+)
+from app.forms import (
+    LoginForm,  # our project customary forms built here to carry out login function.
+    RegistrationForm,  # our project customary forms built here to carry out login function.
+)
+from flask_login import (  # flask_login module is a module to help manage module
+    current_user,  # get the active logged in user.
+    login_user,  # login the user action.
+    logout_user,  # used to logout user
+    login_required,  # used to @login_required decorator to indicate a route MUST be logged in before showing.
+)
 from app.models import User
 from werkzeug.urls import url_parse
-
-# Main root rout.
-@app.route("/")
-
 
 # index page rout.
 @app.route("/")  # this indicate which endpoint these actions will be carried out.
@@ -27,9 +37,24 @@ def index():
     return render_template("index.html", title="Home", posts=posts)
 
 
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {"author": user, "body": "Test post #1"},
+        {"author": user, "body": "Test post #2"},
+    ]
+    return render_template("user.html", user=user, posts=posts)
+
+
 @app.route("/logout")
 def logout():
-    logout_user()
+    """
+    End point to log out the user
+    :return:
+    """
+    logout_user()  # from the flask_login module
     return redirect(url_for("index"))
 
 
@@ -50,19 +75,28 @@ def register():
 
         # Create the user based on the data provided.
         user = User(username=form.username.data, email=form.email.data)
+        # Set the password field entry.
         user.set_password(form.password.data)
 
         # Commit the information to the database.
         db.session.add(user)
         db.session.commit()
+
         # Inform the user.
         flash("Congratulations, you are now a registered user!")
+        # Redirect to login.
         return redirect(url_for("login"))
+
+    # if form not valid, redirect to register page.
     return render_template("register.html", title="Register", form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    """
+    The login endpoint.
+    :return:
+    """
     # Instantiate form data object.
     form = LoginForm()
 
