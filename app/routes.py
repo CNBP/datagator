24078c00dd8f9640bedcce1fb2dee6f33e59bb6d
Yang Ding <dyt811@gmail.com>
2019-09-23 17:05:38 -0400

    Chapter 9 Finished with Pagination Support
    Add basic pagination link

--- app/routes.py
+++ app/routes.py
@@ -51,12 +51,29 @@ def index():
         flash("Your post is now live!")
         return redirect(url_for("index"))
 
-    posts = current_user.followed_posts().all()
+    # Get the page argumetn from the URL entered. Default to 1?
+    page = request.args.get("page", 1, type=int)
+
+    # Use the paginate function from SQLAlchemy to get the posts.
+    posts = current_user.followed_posts().paginate(
+        page, app.config["POSTS_PER_PAGE"], False
+    )
+
+    # Next URL if they exist.
+    next_url = url_for("index", page=posts.next_num) if posts.has_next else None
+
+    # Previous URL if they exist
+    prev_url = url_for("index", page=posts.prev_num) if posts.has_prev else None
 
     # Return rendered template with the variables set.
     # Post > Redirect > Get pattern.
     return render_template(
-        "index.html", title="Home, Sweet Home!", form=form, posts=posts
+        "index.html",
+        title="Home, Sweet Home!",
+        form=form,
+        posts=posts.items,  # must use ITEMS if using pagination
+        next_url=next_url,
+        prev_url=prev_url,
     )
 
 
@@ -68,13 +85,31 @@ def user(username):
     :param username:
     :return:
     """
+    # set user
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {"author": user, "body": "Test post #1"},
-        {"author": user, "body": "Test post #2"},
-    ]
+
+    # set urrent pagination limit
+    page = request.args.get("page", 1, type=int)
+
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config["POSTS_PER_PAGE"], False
+    )
+
+    next_url = (
+        url_for("user", username=user.username, page=posts.next_num)
+        if posts.has_next
+        else None
+    )
+    prev_url = (
+        url_for("user", username=user.username, page=posts.prev_num)
+        if posts.has_prev
+        else None
+    )
+
     # Post > Redirect > Get pattern.
-    return render_template("user.html", user=user, posts=posts)
+    return render_template(
+        "user.html", user=user, posts=posts.items, next_url=next_url, prev_url=prev_url
+    )
 
 
 @app.route("/logout")
@@ -280,5 +315,19 @@ def explore():
     Explore the global posts stream
     :return:
     """
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template("index.html", title="Explore", posts=posts)
+    # Default page argumet is 1. Otherwise, based
+    page = request.args.get("page", 1, type=int)
+
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config["POSTS_PER_PAGE"], False
+    )
+    next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
+
+    return render_template(
+        "index.html",
+        title="Explore",
+        posts=posts.items,  # this must include ITEMS when using pagination
+        prev_url=prev_url,
+        next_url=next_url,
+    )
